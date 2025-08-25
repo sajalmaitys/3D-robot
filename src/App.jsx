e016ba7fbb4e76ef15eb313628890235e4c7fbb1
Pritam <sajalmaity138@gmail.com>
2025-08-25 16:15:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ AOS.init({
 
   return (
     <main>
-      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="Gradient-img" />
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src="./assets/gradient.png" alt="Gradient-img" />
 
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
 
